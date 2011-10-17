75af4291d47abc1de08467a837a59bed1cb9dcff
Furugomu <furugomu@gmail.com>
2011-10-18 07:39:45 +0900

    show replies

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,6 +7,10 @@ class EntriesController < ApplicationController
     @tags = Entry.tag_counts_on(:tags)
   end
 
+  def show
+    @entry = Entry.find(params[:id])
+  end
+
   def new
     @entry = Entry.new()
   end
