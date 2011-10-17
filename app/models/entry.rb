992150ee65091cf1e10f954afa4eab54407fb80b
Furugomu <furugomu@gmail.com>
2011-10-18 04:13:33 +0900

    form

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,7 +6,7 @@ class Entry < ActiveRecord::Base
 
   attr_accessible :photo, :body, :tag_list
 
-  scope :recent, order: {created_at: 'desc'}
+  scope :recent, order: "#{quoted_table_name}.created_at desc"
 
   def tag_list=(tags)
     set_tag_list_on(:tags, tags.strip.split(/\n+/).map(&:strip))
