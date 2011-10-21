e2667a656ff2ff78222525d1e561e0be12aac27b
nsugimoto <nsugimoto@kbmj.com>
2011-10-21 19:09:54 +0900

    hello

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G4up::Application.config.secret_token = 'f3b4d8b975de8148935cc042b02943a63b8b331747617021d1b998aefacea732da2dc8acb89b63efa232172fa0bf68e6e6c02ef2940ff1f83fc6e2506df79cea'
