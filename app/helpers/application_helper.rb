41b536ae1b98149edab60758a70d446e9cfc9625
furugomu <furugomu@gmail.com>
2011-11-15 02:14:29 +0900

    valid html5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # -*- encoding: UTF-8 -*-
 module ApplicationHelper
   def time(time, label, options={})
+    options = options.reverse_merge(datetime: time.iso8601)
     options[:pubdate] = "pubdate" if options[:pubdate]
     content_tag(:time, label, options)
   end
