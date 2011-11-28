a868e0b496d92f3c344aa262f58eccfd3f4b5b5b
furugomu <furugomu@gmail.com>
2011-11-28 23:41:52 +0900

    tag or

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
   def show
     @tag_name = params[:id]
     @title = '「%s」の画像一覧' % @tag_name
-    @entries = Entry.root.tagged_with(params[:id]).
-                 recent.page(params[:page])
+    entries = Entry.root.tagged_with(params[:id], any: params[:any])
+    @entries = entries.recent.page(params[:page])
   end
 end
