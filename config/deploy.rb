61a5f793cb32d61f419f497759c3d7dc5d91fc87
furugomu <furugomu@gmail.com>
2012-07-29 03:47:24 +0900

    新環境用に設定を変えた

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,15 @@
 require 'bundler/capistrano'
 
 # rvm
-set :rvm_type, :user
-$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-require "rvm/capistrano"
+#set :rvm_type, :user
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#require "rvm/capistrano"
 set :rvm_ruby_string, '1.9.2@rails3.1'
+
+#rbenv
+set :default_environment, {
+  'PATH' => '$HOME/.rbenv/shims:$HOME/.rbenv/bin:$HOME/opt/bin:$PATH',
+}
 
 default_run_options[:pty] = true
 set :application, "g4up"
@@ -13,25 +18,9 @@ set :deploy_to, "/var/rails/#{application}"
 set :scm, :git
 set :user, 'faerie'
 
-role :web, "precog.net"                          # Your HTTP server, Apache/etc
-role :app, "precog.net"                          # This may be the same as your `Web` server
-role :db,  "precog.net", :primary => true # This is where Rails migrations will run
+role :web, "takamoriaiko.com"                          # Your HTTP server, Apache/etc
+role :app, "takamoriaiko.com"                          # This may be the same as your `Web` server
+role :db,  "takamoriaiko.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    #run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
-  end
-  after "deploy:symlink", "deploy:s3config"
-  task :s3config do
-    desc "link s3.yaml from shared"
-    run "ln -sf #{shared_path}/config/s3.yaml #{current_path}/config/s3.yaml"
-  end
-end
+require 'capistrano-unicorn'
